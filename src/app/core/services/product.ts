f12c2d82db37ac1e73f4d4b2aafb1161c4c6aeda
Emanuel <emanuelwellygton@gmail.com>
2025-12-30 20:15:12 -0300

    feat: create, update and delete method at ProductService

--- src/app/core/services/product.ts
+++ src/app/core/services/product.ts
@@ -19,4 +19,16 @@ export class ProductService {
   getProductById(id: number): Observable<Product | undefined> {
     return this.http.get<Product>(`${this.apiUrl}/${id}`);
   }
+
+  createProduct(product: Product): Observable<Product> {
+    return this.http.post<Product>(this.apiUrl, product);
+  }
+
+  updateProduct(id: number, product: Product): Observable<Product> {
+    return this.http.put<Product>(`${this.apiUrl}/${id}`, product);
+  }
+
+  deleteProduct(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
 }
