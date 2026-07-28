ee351eb33e52a68cdaeac876f4534dfc0127b267
Emanuel Wellyngton <emanuelwellygton@gmail.com>
2026-07-28 17:47:41 -0300

    feat(routes): Update website routes with additional authentication and information components

--- src/app/website/website.routes.ts
+++ src/app/website/website.routes.ts
@@ -4,9 +4,16 @@ import { ProductDetailsComponent } from '../products/product-details/product-det
 import { ProductListComponent } from '../products/product-list/product-list';
 
 export const websiteRoutes: Routes = [
-    { path: '', component: HomeComponent },
-    { path: 'products', component: ProductListComponent },
-    { path: 'products/:id', component: ProductDetailsComponent },
-    { path: 'cart', loadComponent: () => import('../cart/cart.component').then(m => m.CartComponent) },
-    { path: '**', redirectTo: '' }
+  { path: '', component: HomeComponent },
+  { path: 'products', component: ProductListComponent },
+  { path: 'products/:id', component: ProductDetailsComponent },
+  { path: 'cart', loadComponent: () => import('../cart/cart.component').then(m => m.CartComponent) },
+  { path: 'login', loadComponent: () => import('./auth/login/login.component').then(m => m.SiteLoginComponent) },
+  { path: 'register', loadComponent: () => import('./auth/register/register.component').then(m => m.SiteRegisterComponent) },
+  { path: 'forgot-password', loadComponent: () => import('./auth/forgot-password/forgot-password.component').then(m => m.ForgotPasswordComponent) },
+  { path: 'account', loadComponent: () => import('./account/account.component').then(m => m.AccountComponent) },
+  { path: 'faq', loadComponent: () => import('./faq/faq.component').then(m => m.FaqComponent) },
+  { path: 'about', loadComponent: () => import('./about/about.component').then(m => m.AboutComponent) },
+  { path: 'checkout', loadComponent: () => import('./checkout/checkout.component').then(m => m.CheckoutComponent) },
+  { path: '**', redirectTo: '' }
 ];
